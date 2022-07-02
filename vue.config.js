a58db8f67228d7df7af0b191e599ff0e946e2cfd
dennis0324 <dennis0324@naver.com>
2022-07-02 15:51:09 +0900

    @core-rest and @core-graphQL 의 사용자 정의 디렉토리 추가

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+const path =  require('path')
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+        alias: {
+            '@core-rest': path.join(__dirname, 'src/core/rest'),
+            '@core-graphQL': path.join(__dirname, 'src/core/graphQL'),
+        },
+        extensions: [".js", ".vue", ".json",".ts"],
+    }
+}
 })
