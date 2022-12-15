8e12bd6af6e959194d036057eb52f78620d70822
dennis ko <dennis0324@naver.com>
2022-12-16 00:40:54 +0900

    add: ,  컴포넌트 추가

---
+++ src/components/blogPost/DateIndicator.vue
@@ -0,0 +1,87 @@
+<template>
+    <div id="DateIndicator_Vue">
+        <!-- 깃허브 활동 개수 -->
+        <div id="section-github">
+            <CircleIndicatorVue :contribution-level="level" :type="0"/>
+        </div>
+        <!-- 현재 날짜 -->
+        <div id="section-date" class="center">
+            <div v-if="date == -1" class="date date-deactived"/>
+            <div v-else class="date date-actived center">
+                {{date}}
+            </div>
+        </div>
+        <!-- 블로그 포스팅 개수 -->
+        <div>
+            <span>
+                {{blogPostCount}}
+            </span>
+        </div>
+    </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import CircleIndicatorVue from '../common/CircleIndicator.vue';
+
+export default defineComponent({
+    components:{
+        CircleIndicatorVue
+    },
+    props:{
+        date:{
+            required:true,
+            type : Number,
+            default: -1
+        },
+        level:{
+            required:true,
+            type: String,
+            default:''
+        },
+        blogPostCount:{
+            type:Number
+        }
+    },
+})
+</script>
+
+<style scoped>
+@media(min-width:1000px){
+    #DateIndicator_Vue{
+        display:flex;
+    }
+    #section-date{
+        width:35px;
+        height:35px;
+    }
+    #section-github{
+        height:35px;
+        margin:0 5px 0 5px;
+        display:flex;
+        align-items: center;
+    }
+    .date{
+        background: #202020;
+        font-size:20px;
+        font-weight: bold;
+        color:white;
+        display:flex;
+        
+    /* width: */
+    }
+
+    .date-actived{
+        width:35px;
+        height:35px;
+        border-radius: 30%;
+    }
+    .date-deactived{
+        background: black;
+        width:25px;
+        height:25px;
+        border-radius: 50%;
+    }
+}
+</style>
---
+++ src/components/blogPost/DateSeperator.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="DateSeperator_Vue">
+        <div v-for="(nodeTemp,index) in posts" :key="index">
+            <div v-for="(node,blogPostindex) in nodeTemp" :key="blogPostindex">
+                <DateIndicatorVue :date="(Number(node.updatedat.split('-')[2]) as number)"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { BlogPostData } from '@/utils/Types';
+import { defineComponent, PropType } from 'vue'
+import DateIndicatorVue from './DateIndicator.vue';
+export default defineComponent({
+    components:{
+        DateIndicatorVue
+    },
+    props:{
+        posts:{
+            required:true,
+            type: Object as PropType<BlogPostData[][]>
+        }
+    },
+    watch:{
+        posts(){
+            console.log(this.posts);
+        }
+    },
+    mounted(){
+        
+        console.log(this.posts[0][0].updatedat.split('-')[2])
+    }
+    // setup() {
+
+    // }
+})
+</script>
+
+<style scoped>
+
+@media (min-width : 1000px) {
+        .logo{
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display:flex;
+
+        }
+    }
+/* 로고(핸드폰) */
+@media (max-width: 999px) {
+    .logo{
+        display:block;
+    }
+}
+
+@media (max-width: 600px){
+    .logo-pic:not(.home) {
+        display:none !important;
+    }
+}
+
+#DateSeperator_Vue{
+
+}
+</style>
